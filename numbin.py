21bedfed97afd0a2de71f7c6fee135029ca0a203
Levi (dylan) <128552152+dylanabzr@users.noreply.github.com>
2023-04-24 12:13:01 -0300

    Limpeza de código.
    Adição de uma função, nomes de variáveis mais entendíveis, código mais bem organizado, substituição da forma em que os numeros eram imprimidos e redução de linhas.

--- numbin.py
+++ numbin.py
@@ -1,43 +1,38 @@
-B = [ ]
-D = [ ]
-num = input("Digite um número decimal:")
-num = num.replace(".", " ")
-num = num.split()
-num[0] = int(num[0])
-if len(num) != 1:
-	num[1] = "0." + num[1]
-	num[1] = float(num[1])
-while True:
-	resto = num[0] % 2
-	B.append(resto)
-	quociente = num[0] // 2
-	if quociente == 1:
-		B.append(quociente)
-		break
-	num[0] = quociente
-x = len(B)
-if len(num) != 1:
+def conversor_para_binario(num):
+	digitos_inteiros = []
+	digitos_decimais = []
+	num = num.replace(".", " ")
+	num = num.split()
+	num[0] = int(num[0])
+	if len(num) != 1:
+		num[1] = "0." + num[1]
+		num[1] = float(num[1])
 	while True:
-		resto = num[1] * 2
-		if resto > 1:
-			D.append(1)
-			resto -= 1
-			num[1] = resto
-		elif resto < 1:
-			D.append(0)
-			num[1] = resto
-		elif resto == 1:
-			D.append(1)
+		resto = num[0] % 2
+		digitos_inteiros.append(resto)
+		quociente = num[0] // 2
+		if quociente == 1:
+			digitos_inteiros.append(quociente)
 			break
-y = 0
-for i in range(x):
-	x -= 1
-	print(f"{B[x]}",end="")
-if len(num) != 1:
-	print(".",end="")
-	while True:
-		print(f"{D[y]}",end="")
-		if y == (len(D) - 1):
-			break
-		y += 1
-print()
+		num[0] = quociente
+	if len(num) != 1:
+		while True:
+			resto = num[1] * 2
+			if resto > 1:
+				digitos_decimais.append(1)
+				resto -= 1
+				num[1] = resto
+			elif resto < 1:
+				digitos_decimais.append(0)
+				num[1] = resto
+			elif resto == 1:
+				digitos_decimais.append(1)
+				break
+	digitos_inteiros = "".join(map(str, digitos_inteiros))
+	digitos_decimais = "".join(map(str, digitos_decimais))
+	digitos = digitos_inteiros + "." + digitos_decimais
+	return digitos
+
+
+a = input("Digite um número decimal para ser convertido para binário: ")
+print(conversor_para_binario(a))
